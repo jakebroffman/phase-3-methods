b42bd2e6b9d2adc019fcc04db81c4bb273efb7c6
jakebroffman <jake.broffman1088@gmail.com>
2023-05-28 10:57:33 -0400

    commit tree 69aa9ad7f172cbed7d1cb52501275d4110f82807

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num != Integer
+        return nil
+    end
+    if num = Integer
+        return num / 2
+    end
+end
+
+halve(6)
+
+
+add(2,5)
+
+greet_with_default("Naureen")
+
+greet("Naureen")
+
+greet_programmer
